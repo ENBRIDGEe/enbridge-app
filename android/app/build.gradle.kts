54855b110a8879f15910ae7ad7763268f097823c
Jothik Kanth <vanamjothik@gmail.com>
2026-06-11 11:16:06 +0530

    Chore: Upgrade targetSdk to 35 and bump version to 1.0.0+2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
     defaultConfig {
         applicationId = "com.navneet.enbridge"
         minSdk = flutter.minSdkVersion
-        targetSdk = 34
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
